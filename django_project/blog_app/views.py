9a902d3ba30b88d7df5cdd6d6b5355c176918686
Nearo007 <araujo8166@gmail.com>
2025-03-18 13:33:34 -0300

    add: profile page, see your own posts.

--- django_project/blog_app/views.py
+++ django_project/blog_app/views.py
@@ -93,7 +93,8 @@ def create_post(request):
 
 @login_required(login_url='login')
 def user_profile(request):
-    return render(request, 'user_profile.html')
+    posts = Post.objects.all().filter(author=request.user).order_by('-date')
+    return render(request, 'user_profile.html', {'posts': posts})
 
 @login_required(login_url='login')
 def post_page(request, post_id):
